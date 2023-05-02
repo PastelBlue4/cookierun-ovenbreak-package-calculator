d533377d787f1c55e0745b81ecedc791dd4c05a4
Romuru <pastelblue0721@gmail.com>
2023-05-02 23:36:37 +0900

    feat footer contents

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="w-full max-w-md ">
-      <div className="flex items-center justify-around h-full text-xs">
+    <div className="w-full max-w-md py-5 bg-blue-200">
+      <div className="flex items-center justify-around text-sm">
         <button>도움이 되었나요?</button>
         <button>계산 기준 확인하기</button>
+      </div>
+      <div className="flex items-center justify-center mt-4">
+        <span className="text-xs">버그 제보 : pastelblue0721@gmail.com</span>
       </div>
     </div>
   );
