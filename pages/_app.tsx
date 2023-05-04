acfa074a0c5b6f8cf367bf38b1f9e98707738e91
Romuru <pastelblue0721@gmail.com>
2023-05-05 00:45:04 +0900

    feat Analytics components add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <div className="flex flex-col items-center justify-between min-h-screen bg-blue-100">
         <Component {...pageProps} />
+        <Analytics />
         <Footer />
       </div>
     </RecoilRoot>
