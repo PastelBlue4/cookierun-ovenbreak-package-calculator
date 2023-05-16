3709705133f774b958bd583b2e5d354acdaa7fb6
Romuru <pastelblue0721@gmail.com>
2023-05-16 12:32:22 +0900

    feat. donation ui move location.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,10 @@
 import React, { useState } from "react";
-import Modal from "./Modal";
+
 import Link from "next/link";
 
 export default function Footer() {
-  const [isDonationModalOpen, setIsDonationModalOpen] = useState(false);
-
   return (
     <>
-      <Modal
-        isOpen={isDonationModalOpen}
-        setIsOpen={setIsDonationModalOpen}
-        title="아가 쿠키 커피사주기"
-        contents={{
-          text: `응애 "사줘"`,
-          image: "/giveMeMore.png",
-          donationLink: "https://toss.me/romuru",
-        }}
-      />
       <div className="relative bottom-0 flex flex-col items-center w-full py-3 bg-blue-200 ">
         <div className="w-full max-w-md">
           <div className="flex flex-col items-center justify-center mb-2">
@@ -36,9 +24,6 @@ export default function Footer() {
           </div>
 
           <div className="flex items-center justify-around w-full my-3 text-xs">
-            <button onClick={() => setIsDonationModalOpen(true)}>
-              도움이 되었나요?
-            </button>
             <Link
               href="https://github.com/PastelBlue4/cookierun-ovenbreak-package-calculator"
               target="blank"
